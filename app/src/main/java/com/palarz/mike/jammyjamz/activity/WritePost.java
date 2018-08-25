f12038214e755c4bbf3f6eb887367da6bedab464
Mike Palarz <mpalarz1@gmail.com>
2018-08-25 13:38:53 -0500

    feat: Adjusted EditText in WritePost
    I also didn't like how the EditText looked in WritePost. It now
    has a character counter and a hint that gets animated when the
    user clicks on the EditText. Sweet!

--- app/src/main/java/com/palarz/mike/jammyjamz/activity/WritePost.java
+++ app/src/main/java/com/palarz/mike/jammyjamz/activity/WritePost.java
@@ -2,9 +2,13 @@ package com.palarz.mike.jammyjamz.activity;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,7 +27,8 @@ public class WritePost extends AppCompatActivity {
     public static final String EXTRA_CONTENT = "com.palarz.mike.jammyjamz.activity.WritePost.extra_content";
 
     // Contains the message that will be added to the post
-    private EditText mMessage;
+    private TextInputLayout mMessageContainer;
+    private TextInputEditText mMessage;
     private ImageButton mSendButton;
     // Post object that was received from PostSearch
     private Post mPost;
@@ -36,7 +41,8 @@ public class WritePost extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.write_post_toolbar);
         setSupportActionBar(toolbar);
 
-        mMessage = (EditText) findViewById(R.id.write_post_post_message);
+        mMessageContainer = findViewById(R.id.write_post_post_message_container);
+        mMessage = findViewById(R.id.write_post_post_message);
 
         Intent receivedIntent = getIntent();
         if (receivedIntent != null && receivedIntent.hasExtra(EXTRA_CONTENT)){
@@ -69,6 +75,8 @@ public class WritePost extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
 
+            // TODO: Need to add a button for posting the jam!!!
+
             case R.id.newsfeed_menu_action_sign_out:
                 // TODO: Not the most elegant solution, try to think of something better
                 AuthUI.getInstance()
@@ -77,6 +85,7 @@ public class WritePost extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
                                 startActivity(new Intent(WritePost.this, Newsfeed.class));
+                                finish();
                             }
                         });
                 return true;
